5e8be1c64e5054fd731e38a55718eb813a958670
vilabril <vilabril@gmail.com>
2021-05-10 17:21:28 -0300

    DepartmentForm (dialog) design

--- src/graficUserInterface/DepartamentListController.java
+++ src/graficUserInterface/DepartamentListController.java
@@ -1,18 +1,27 @@
 package graficUserInterface;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Departament;
 import model.services.DepartamentService;
@@ -36,8 +45,10 @@ public class DepartamentListController implements Initializable {
 	private ObservableList<Departament> obsList;
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		/*createDialogForm("/graficUserInterface/About.fxml", parentStage);*/
+		createDialogForm("/graficUserInterface/DepartmentForm.fxml", parentStage);
 	}
 	
 	public void setDepartamentService (DepartamentService service) {
@@ -68,5 +79,22 @@ public class DepartamentListController implements Initializable {
 		 obsList = FXCollections.observableArrayList(list);
 		 tableViewDepartament.setItems(obsList);
 	}
-
+	
+	private void createDialogForm( String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader  = new FXMLLoader (getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch (IOException e){
+			Alerts.showAlert("IOEXception", "Error loading View", e.getMessage(), AlertType.ERROR);
+			
+		}
+	}
 }
